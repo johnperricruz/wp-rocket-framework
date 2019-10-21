807425f50ce3ee5f2ad3e4fd0886a81918467912
John Perri Cruz <johnperri.cruz@acidgreen.com.au>
2019-10-21 15:23:53 +0800

    v1.33 : Added template changes

--- templates/full-width.php
+++ templates/full-width.php
@@ -4,22 +4,20 @@
  *
  */
 get_header(); ?>
-    <div id="primary" class="full-width site-content">
-        <header class="innerpage-header p-5">
-            <h1 class="text-white text-center innerpage-title mb-5">
-                <?php the_title(); ?>
-            </h1>
-            <?php
-                if ( function_exists('yoast_breadcrumb') ) {
-                  yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-                }
-            ?>
-        </header> 
-        <?php 
-            while (have_posts()){ 
-                the_post(); 
-                the_content();
-            } 
-        ?>                  
-    </div><!-- .primary -->
+    <header class="innerpage-header p-5">
+        <h1 class="text-white text-center innerpage-title mb-5">
+            <?php the_title(); ?>
+        </h1>
+        <?php
+            if ( function_exists('yoast_breadcrumb') ) {
+              yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+            }
+        ?>
+    </header> 
+    <?php 
+        while (have_posts()){ 
+            the_post(); 
+            the_content();
+        } 
+    ?>                  
 <?php get_footer(); ?>
